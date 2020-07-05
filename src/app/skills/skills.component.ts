a4a18f8ed6297ed47322dfb41f75ab28f78f8b9e
Andrei Voicu <dreivoicu@gmail.com>
2020-07-05 17:36:45 +0300

    basic methods

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,13 +7,29 @@ import { FormArray, FormControl } from '@angular/forms';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-  skills = new FormArray([]);
+  skills = new FormArray([
+    new FormControl('Reactive'),
+    new FormControl('Array'),
+    new FormControl('Form')
+  ]);
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  prepend(): void {
+    this.skills.insert(0, new FormControl(''));
+  }
+
   addSkill(): void {
     this.skills.push(new FormControl(''));
+  }
+
+  removeSkill(index: number): void {
+    this.skills.removeAt(index);
+  }
+
+  clear(): void {
+    this.skills.clear();
   }
 }
